3aa74a6f2ea0ca92b3599a6eaf47eb125a3e33d4
Anik-08 <anik.tiwary08@gmail.com>
2025-04-23 12:15:58 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const config = {
+  output: 'export',
+  distDir: 'dist',
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+export default config
